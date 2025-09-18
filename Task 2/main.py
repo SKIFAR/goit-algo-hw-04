dacd2e7f295b2f9d1d604da3486fa4a260c39b11
Valerii Odintsov <skif1998@gmail.com>
2025-09-18 17:19:23 +0200

    Task 2

---
+++ Task 2/main.py
@@ -0,0 +1,30 @@
+import turtle
+
+def koch_curve(t, order, size):
+    if order == 0:
+        t.forward(size)
+    else:
+        for angle in [60, -120, 60, 0]:
+            koch_curve(t, order - 1, size / 3)
+            t.left(angle)
+
+def draw_koch_curve(order, size=300):
+    window = turtle.Screen()
+    window.bgcolor("black")
+
+    t = turtle.Turtle()
+    t.color("white")
+    t.speed(0)  
+    t.penup()
+    t.goto(-size / 2, size / 3)
+    t.pendown()
+
+    for _ in range(3):
+        koch_curve(t, order, size)
+        t.right(120)
+
+    t.hideturtle()
+    window.mainloop()
+
+# Test
+draw_koch_curve(3)
